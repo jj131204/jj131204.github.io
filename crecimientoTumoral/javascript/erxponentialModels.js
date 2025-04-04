6687eb5c56a8a4c4f78a884258b4c9b6c0d51df6
Juan José Arteta <77931977+jj131204@users.noreply.github.com>
2025-04-03 20:43:57 -0500

    crecimiento tumoral

---
+++ crecimientoTumoral/javascript/erxponentialModels.js
@@ -0,0 +1,74 @@
+let chart = null; 
+function verificarCrecimiento(){
+
+    let tumorChart = document.getElementById("tumorChart");
+    let celulasIniciales = document.getElementById("celulasIniciales").value;
+    let tasaDeCrecimiento = document.getElementById("tasaDeCrecimiento").value;
+    let diasGraficados = document.getElementById("diasGraficados").value;
+
+    if(celulasIniciales != "" && tasaDeCrecimiento != "" && diasGraficados != ""){
+        tumorChart.style.display = "flex";
+
+        const N0 = parseFloat(celulasIniciales); // Células iniciales
+        const r = parseFloat(tasaDeCrecimiento);     // Tasa de crecimiento diaria
+        const dias = parseInt(diasGraficados);    // Número de días a graficar
+
+        const labels = [];
+        const datos = [];
+
+        for (let t = 0; t <= dias; t++) {
+            labels.push(`Día ${t}`);
+            const Nt = N0 * Math.exp(r * t);
+            datos.push(Math.round(Nt));
+        }
+
+        // 🔹 Destruir gráfico anterior si existe
+        if (chart) {
+        chart.destroy();
+        }
+
+        const ctx = document.getElementById('tumorChart').getContext('2d');
+        chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                label: 'Número de células tumorales',
+                data: datos,
+                borderColor: 'rgba(49,148,210, 1)',
+                backgroundColor: 'rgba(49,148,210, 0.2)',
+                fill: true,
+                tension: 0.3
+                }]
+            },
+            options: {
+                responsive: false,
+                scales: {
+                y: {
+                    beginAtZero: false,
+                    title: {
+                    display: true,
+                    text: 'Células'
+                    }
+                },
+                x: {
+                    title: {
+                    display: true,
+                    text: 'Días',
+                    },
+                    ticks: {
+                        // stepSize = 1
+                    }
+                }
+                }
+            }
+        });
+    }else{
+        tumorChart.style.display = "none";
+        console.log("Ingresa todos los valores...");
+    }
+
+    
+
+    
+}
